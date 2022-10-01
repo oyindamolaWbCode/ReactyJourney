ff3ff942fc862d470337e218eaa654d0c4f611ba
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2022-10-01 02:22:54 +0200

    map issue

--- reacty/src/App.js
+++ reacty/src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
 import Header from "./components/Header";
-import Content from "./components/Content";
+//import Content from "./components/Content";
 import Footer from "./components/Footer";
+import Employees from "./components/Employees";
 import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="content">
-      <h3>God Abeg</h3>
+      <div>
       <Header />
-      <Content />
+      <Employees />
       <Footer />
+      </div>
       <Routes>
         <Route path="/header" element={<Header />} />
-        <Route path="/content" element={<Content />} />
+        {/* <Route path="/content" element={<Content />} /> */}
         <Route path="/footer" element={<Footer />} />
+        <Route path="/employees" element={<Employees />} />
       </Routes>
     </div>
   );
--- reacty/src/components/Content.js
+++ reacty/src/components/Content.js
@@ -1,9 +1,9 @@
-const Content = () => {
-    return ( 
-        <div className="theContent">
-            <h2>The content body is Here !</h2>
-        </div>
-     );
-}
+// const Content = () => {
+//     return ( 
+//         <div className="theContent">
+//             <h2>The content body is Here !</h2>
+//         </div>
+//      );
+// }
  
-export default Content;
+// export default Content;
